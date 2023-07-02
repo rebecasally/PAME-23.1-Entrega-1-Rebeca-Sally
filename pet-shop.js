4f7953db70741e2e6f7019ddccbec71f3fdffa8e
rebecasally <rebecasallygiuli@gmail.com>
2023-07-02 15:39:10 -0300

    entrega parcial

--- pet-shop.js
+++ pet-shop.js
@@ -4,20 +4,12 @@ const prompt = require("prompt-sync")({sigint:true});
 
 class Cliente{
 
-    constructor(idCliente,nomeCliente,pets,fidelizado){
+    constructor(nomeCliente,idCliente,pets,fidelizado){
 
-        this.idCliente = idCliente;
         this.nomeCliente = nomeCliente;
+        this.idCliente = idCliente;
         this.pets = pets;
         this.fidelizado = fidelizado;
-    }
-
-    get idCliente(){
-        return this._idCliente;
-    }
-
-    set idCliente(novoIdCliente){
-        this._id = novoIdCliente;
     }
 
     get nomeCliente(){
@@ -28,12 +20,20 @@ class Cliente{
         this._nomeCliente= novoNomeCliente;
     }
 
+    get idCliente(){
+        return this._idCliente;
+    }
+
+    set idCliente(novoIdCliente){
+        this._idCliente = novoIdCliente;
+    }
+
     get pets(){
         return this._pets;
     }
 
     set pets(novoPet){
-        this._pets=pets+novoPet;
+        this._pets=novoPet;
     }
 
     get fidelizado(){
@@ -49,12 +49,12 @@ class Cliente{
 
 class Funcionario{
 
-    #senha;
+    #senhaFunc;
 
-    constructor(idFunc,nomeUsuario,senha){
+    constructor(idFunc,nomeUsuario,senhaFunc){
         this.idFunc = idFunc;
         this.nomeUsuario = nomeUsuario;
-        this.#senha= senha;
+        this.#senhaFunc= senhaFunc;
     }
 
     get idFunc(){
@@ -73,12 +73,12 @@ class Funcionario{
         this._nomeUsuario = novoNomeUsuario;
     }
 
-    get senha(){
-        return this.#senha;
+    get senhaFunc(){
+        return this.#senhaFunc;
     }
 
-    set senha(novoSenha){
-        this.#senha= novoSenha;
+    set senhaFunc(novoSenhaFunc){
+        this.#senhaFunc= novoSenhaFunc;
     }
 
 }
@@ -86,9 +86,9 @@ class Funcionario{
 
 class Animal extends Cliente{
 
-    constructor(idPet,nomeCliente,nomePet,consultas){
+    constructor(nomeCliente,idPet,nomePet,consultas){
         super(nomeCliente)
-        this.id = idPet;
+        this.idPet = idPet;
         this.nomePet = nomePet;
         this.consultas= consultas;
     }
@@ -98,7 +98,7 @@ class Animal extends Cliente{
     }
 
     set idPet(novoIdPet){
-        this._id = novoIdPet;
+        this._idPet = novoIdPet;
     }
 
     get nomePet(){
@@ -114,10 +114,11 @@ class Animal extends Cliente{
     }
 
     set consultas(novoConsulta){
-        this._consultas= consultas+novoConsulta;
+        this._consultas= novoConsulta;
     }
 
 }
+
 
 class Consultas extends Funcionario{
 
@@ -163,39 +164,130 @@ class Consultas extends Funcionario{
 
 }
 
+
 class Sistema{
 
     #senha; 
 
-    constructor(usuario,senha){
-        this.usuario=usuario;
-        this.#senha=senha;
+    constructor(funcionarios,clientes,animais,consultas,ids){
+        this.funcionarios=funcionarios;
+        this.clientes=clientes;
+        this.animais=animais;
+        this.consultas=consultas;
+        this.ids=ids;
     }
 
-    get usuario(){
-        return this._usuario;
+    get funcionarios(){
+        return this._funcionarios;
+    }
+    set funcionarios(novoFuncionarios){
+        this._funcionarios=novoFuncionarios;
     }
 
-    set usuario(novoUsuario){
-        this._usuario=novoUsuario;
+
+    get clientes(){
+        return this._clientes;
+    }
+    set clientes(novoClientes){
+        this._clientes= novoClientes;
+    }
+
+    get animais(){
+        return this._animais;
+    }
+    set animais(novoAnimais){
+        this._animais=novoAnimais;
     }
 
 
-    get senha(){
-        return this.#senha;
+    get consultas(){
+        return this._consultas;
+    }
+    set consultas(novoConsultas){
+        this._consultas=novoConsultas;
+    }
+    
+    get ids(){
+        return this._ids;
+    }
+    set ids(novoId){
+        this._ids=novoId;
     }
 
-    set senha(novoSenha){
-        this.#senha= novoSenha;
+
+    mostrarDados(usuario){
+
+        let achado=0;
+
+        for (let n in this.funcionarios){
+            if (this.funcionarios[n].nomeUsuario==usuario){
+                achado=1;
+            }
+        }
+
+        if (achado==1){
+            let usuarioAtual = this.funcionarios.find(funcionario => funcionario.nomeUsuario=== usuario);
+            console.log("Usuário:",usuarioAtual.nomeUsuario);
+            console.log("Id:",usuarioAtual.idFunc);
+            console.log("Senha:",usuarioAtual.senha);
+        }
+        else {
+            console.log("Usuário não encontrado!")
+        }
+
+    }
+
+    alterarDados(usuario,senha){
+        let novoUsuario=usuario;
+        let novaSenha=senha;
+        let usuarioAtual = this.funcionarios.find(funcionario => funcionario.nomeUsuario=== usuario);
+
+        console.log("\nQual dado deseja alterar?");
+        console.log("\n1- Nome de usuário");
+        console.log("\n2- Senha\n")
+
+        let d= parseInt(prompt("Escolha uma opção: "));
+
+        if (d==1){
+            novoUsuario=prompt("Digite o novo nome de usuario: ");
+            usuarioAtual.nomeUsuario=novoUsuario;                  
+            }
+
+        else if (d==2){
+            novaSenha=prompt("Digite a nova senha: ");
+            usuarioAtual.senhaFunc=novaSenha;                   
+            }
+
+        else {
+            console.log("\nOpção inválida!\n");
+            }
+
+        return [novoUsuario,novaSenha]
+
+    }
+
+    listaClientes(){
+        let nomeClientes=[];
+        for (let n in this.clientes){
+            nomeClientes.push(this.clientes[n].nomeCliente);
+        }
+
+        nomeClientes = nomeClientes.sort();
+
+        for (let x in nomeClientes){
+            console.log(nomeClientes[x]);
+        }
     }
 
 }
 
+
 function gerarId(){
     const min=1000
     const max=9999
-    let id = Math.floor(Math.random() * (max - min + 1) ) + min;
-    return id
+    let id;
+    id = Math.floor(Math.random() * (max - min + 1) ) + min;
+    return id;
 }
 
 
@@ -218,21 +310,53 @@ function menuLogado() {
     console.log('10- Remover Pet');
     console.log('11- Cancelar Consulta');
     console.log('12- Remover Funcionario');
-    console.log('13- Sair do Programa');
+    console.log('13- Fazer Logout');
 
 }
+
+
+function criarClientes(sistema){
+    let id1= gerarId();
+    let id2= gerarId();
+    let id3= gerarId();
+        
+    let cliente1= new Cliente('maria',id1,['farofa','banana'],0);
+    let cliente2= new Cliente('fernanda',id2,['tchutchuca'],0);
+    let cliente3= new Cliente('zoe',id3,['maricota'],0);
+
+    sistema.clientes.push(cliente1);
+    sistema.clientes.push(cliente2);
+    sistema.clientes.push(cliente3);
+
+}
+
+function criarPets(sistema){
+    let id1= gerarId();
+    let id2= gerarId();
+    let id3= gerarId();
+    let id4= gerarId();
+            
+    let pet1= new Animal('maria',id1,'farofa',[]);
+    let pet2= new Animal('maria',id2,'banana',[]);
+    let pet3= new Animal('fernanda',id3,'tchutchuca',[]);
+    let pet4= new Animal('zoe',id4,'maricota',[]);
+    
+    sistema.clientes.push(pet1);
+    sistema.clientes.push(pet2);
+    sistema.clientes.push(pet3);
+    sistema.clientes.push(pet4);
+    }
 
 function Programa(){
     //funcao principal do programa
     let op1;
     let op2;
-    let funcionarios=[];
-    let consultas=[];
-    let clientes=[];
-    let animais=[];
-
 
     console.log('\nBem Vindo ao Pet Shop!\n');
+
+    let sistema=new Sistema([],[],[],[],[0]);
+    criarClientes(sistema);
+    criarPets(sistema);
 
     while (true){
 
@@ -241,6 +365,7 @@ function Programa(){
         op1 = parseInt(prompt('Escolha uma das opções acima para continuar: '));
 
         if (op1==1){
+            //Fazendo o Login
             
             console.log('\n')
             let usuario = prompt("Digite o nome de usuário: ");
@@ -249,25 +374,22 @@ function Programa(){
             //Verificando se o usuário existe
             let achado=0;
 
-            for (let i in funcionarios){
-                console.log(funcionarios);
-
-                if (funcionarios[i].nomeUsuario==usuario){
-                    achado=1;
+            for (let i in sistema.funcionarios){
+                if (sistema.funcionarios[i].nomeUsuario==usuario){
+                    if (sistema.funcionarios[i].senhaFunc==senha){
+                        achado=1
+                    }
                 }
             }
 
             if (achado!=1){
-                console.log("\nUsuário não encontrado!\n")
+                console.log("\nUsuário não encontrado ou senha incorreta.\n")
             }
 
             else {
-            
-                const login = new Sistema(usuario,senha);
-            
                 console.log('\nLogin feito com sucesso!');
 
-                let usuarioAtual = funcionarios.find(funcionario => funcionario.nomeUsuario=== login.usuario);
+                let usuarioAtual = sistema.funcionarios.find(funcionario => funcionario.nomeUsuario=== usuario);
     
                 let sair=0;
     
@@ -277,52 +399,18 @@ function Programa(){
     
                     switch(op2-1){
                         case 0:
-                            console.log("Usuário:",usuarioAtual.nomeUsuario);
-                            console.log("Id:",usuarioAtual.idFunc);
-                            console.log("Senha:",usuarioAtual.senha);
-    
+                            sistema.mostrarDados(usuario);
                             break
                         
                         case 1:
-                            console.log("\nQual dado deseja alterar?");
-                            console.log("\n1- Nome de usuário");
-                            console.log("\n2- Senha\n")
-
-                            let d;
-
-                            d= parseInt(prompt("Escolha uma opção: "));
-
-                            if (d==1){
-                                let novoUsuario=prompt("Digite o novo nome de usuario: ");
-                                usuarioAtual.nomeUsuario=novoUsuario;
-                                
-                            }
-
-                            else if (d==2){
-                                let novaSenha=prompt("Digite a nova senha: ");
-                                usuarioAtual.senha=novaSenha;
-                                
-                            }
-
-                            else {
-                                console.log("\nOpção inválida!\n");
-                            }
-
+                            let novosDados = sistema.alterarDados(usuario,senha);
+                            usuario=novosDados[0];
+                            senha=novosDados[1];
 
                             break
 
                         case 2:
-                            let nomeClientes=[];
-                            for (let n in clientes){
-                                nomeClientes.push(clientes[n].nomeCliente);
-                            }
-
-                            nomeClientes = nomeClientes.sort()
-
-                            for (x in nomeClientes){
-                                console.log("\n",nomeClientes[x]);
-                            }
-                            
+                            sistema.listaClientes();
                             break
 
                         case 3:
@@ -343,13 +431,13 @@ function Programa(){
         }
 
         else if (op1==2){
-            let idFunc= gerarId()
+            let id= gerarId();
             let usuario = prompt('Digite o nome de usuário: ');
             let senha = prompt('Digite a senha: ');
 
 
-            const funcionario = new Funcionario(idFunc,usuario,senha);
-            funcionarios.push(funcionario);
+            const funcionario = new Funcionario(id,usuario,senha);
+            sistema.funcionarios.push(funcionario);
             console.log("\nCadastro realizado com sucesso!");
         }
 
